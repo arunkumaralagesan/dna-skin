d59df42f51b8ff249a7d9dd4e0a993e115240a54
arunkumaralagesan <arunkumara300@gmail.com>
2021-09-14 23:05:09 +0530

    Stable form validation

--- src/pages/home/components/appointmentForm/AppointmentForm.js
+++ src/pages/home/components/appointmentForm/AppointmentForm.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import _get from 'lodash/get';
+import _get from "lodash/get";
 import { DatePicker, Select } from "antd";
 
 import { isMobileDevice } from "../../../../helpers/utils";
 import "./appointmentForm.css";
+import { isValidEmail, validateFormHelper } from './appointmentForm.helpers';
 
 const { Option } = Select;
 
@@ -12,92 +13,145 @@ export class AppointmentForm extends Component {
     super(props);
     this.state = {
       formValues: {},
+      displayError: false,
+      formErrors: {},
     };
   }
 
-  renderInputFiled(name, key, type = "text", required = true) {
+  renderError = (key) => {
+    const { displayError, formErrors } = this.state;
+    if (!displayError || !formErrors[key]) return null;
+
+    return <div className="input-error">*{formErrors[key]}</div>;
+  };
+
+  renderInputFiled(name, key, type = "text", required = false) {
     const { formValues } = this.state;
     return (
-      <input
-        className="input input-field"
-        type={type}
-        name={name}
-        placeholder={name}
-        required={required}
-        value={formValues[key]}
-        onChange={evt => this.setState({ formValues: { ...formValues, [key]: evt.target.value }})}
-      />
+      <div className="input-field-wrapper">
+        <input
+          className="input input-field"
+          type={type}
+          name={name}
+          placeholder={name}
+          required={required}
+          value={formValues[key]}
+          onChange={(evt) =>
+            this.setState({
+              formValues: { ...formValues, [key]: evt.target.value },
+            }, () => this.validateForm())
+          }
+        />
+        {this.renderError(key)}
+      </div>
     );
   }
 
   renderPhone() {
+    const { formValues } = this.state;
     return (
-      <input
-        className="input input-field"
-        type="number"
-        name="phone_number"
-        placeholder="Phone Number"
-        min="1111111111"
-        max="99999999999999"
-        required
-        value={this.state["phone_number"]}
-      />
+      <div className="input-field-wrapper">
+        <input
+          className="input input-field"
+          type="text"
+          name="phone_number"
+          placeholder="Phone Number"
+          maxlength="10"
+          value={formValues.phone_number}
+          onChange={(evt) => {
+            const phone = evt.target.value;
+            this.setState({
+              formValues: {
+                ...formValues,
+                phone_number: isNaN(phone)
+                  ? formValues.phone_number || ""
+                  : evt.target.value,
+              },
+            }, () => this.validateForm());
+          }}
+        />
+        {this.renderError("phone_number")}
+      </div>
     );
   }
 
+  disabledDate(current) {
+    return current && current.valueOf() < Date.now();
+  }
+
   renderSelectAndDate() {
+    const { formValues } = this.state;
     return (
       <div className="form__row">
-        {this.renderInputFiled(
-          "Email address",
-          "email_address",
-          "email",
-          true
-        )}
-        <div className="form__date-and-select">
-          <DatePicker
-            onChange={(props) => console.log(props)}
-            className="form__date-picker"
-          />
-          <div className="form__date-separator"></div>
-          <Select
-            bordered={false}
-            placeholder="Treatment"
-            dropdownClassName="form__appointment-select"
-            style={{ width: 150 }}
-            onChange={(props) => console.log(props)}
-          >
-            <Option value="Skin">Skin</Option>
-            <Option value="Hair">Hair</Option>
-            <Option value="Body">Body</Option>
-          </Select>
+        {this.renderInputFiled("Email address", "email")}
+        <div className="input-field-wrapper">
+          <div className="form__date-and-select">
+            <DatePicker
+              disabledDate={this.disabledDate}
+              onChange={(evt, dateString) =>
+                this.setState({
+                  formValues: { ...formValues, date: dateString },
+                }, () => this.validateForm())
+              }
+              className="form__date-picker"
+              format="DD/MM/YYYY"
+            />
+            <div className="form__date-separator"></div>
+            <Select
+              bordered={false}
+              placeholder="Treatment"
+              dropdownClassName="form__appointment-select"
+              style={{ width: 150 }}
+              onChange={val =>
+                this.setState({
+                  formValues: { ...formValues, appointment: val },
+                }, () => this.validateForm())
+              }
+            >
+              <Option value="Skin">Skin</Option>
+              <Option value="Hair">Hair</Option>
+              <Option value="Body">Body</Option>
+            </Select>
+          </div>
+          {this.renderError("date_appointment")}
         </div>
       </div>
     );
   }
 
-  handleSubmit() {
-    const { formValues } = this.state;
-    const name = _get(formValues, 'person_name');
-    const phone = _get(formValues, 'phone_number');
-    const date = _get(formValues, 'date');
-    const appointment = _get(formValues, 'appointment');
-    const query = _get(formValues, 'query');
-    if (name && phone && date && appointment && query) {
+  validateForm = () => {
+    const { formValues, displayError } = this.state;  
+    const formErrors = validateFormHelper({ formValues, displayError });
+    this.setState({ formErrors });
+  };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    this.setState({ displayError: true }, () => this.validateForm());
+    
+    const { formValues, formErrors } = this.state;
+    if (Object.values(formErrors).some((value) => value)) {
+
+    } else {
+      const name = _get(formValues, "person_name");
+      const phone = _get(formValues, "phone_number");
+      const date = _get(formValues, "date");
+      const appointment = _get(formValues, "appointment");
+      const query = _get(formValues, "query");
+
+      // handle Submit
     }
-  }
+  };
 
   render() {
     const isMobile = isMobileDevice();
-    const { formValues } = this.state;
-    console.log({ formValues })
+    const { formValues, formErrors } = this.state;
+    console.log({ formErrors });
     // if (isMobile) return null;
     return (
       <div className="form-container">
-        <div class="form__title">
-          Request an Appointment
-        </div>
+        <div class="form__title">Request an Appointment</div>
         <div class="form__contact">
           Call Us:- +91 73380 10101, +91 73384 44555
         </div>
@@ -105,8 +159,6 @@ export class AppointmentForm extends Component {
           id="ContactusForm"
           name="contact-form1"
           onSubmit={this.handleSubmit}
-          // action="thank-you?form=contact_form"
-          // method="POST"
           className="appointment-form"
         >
           <div className="form__row">
@@ -121,7 +173,11 @@ export class AppointmentForm extends Component {
             cols="20"
             placeholder="Query/Message"
             rows="3"
-            onChange={evt => this.setState({ formValues: { ...formValues, query: evt.target.value }})}
+            onChange={(evt) =>
+              this.setState({
+                formValues: { ...formValues, query: evt.target.value },
+              })
+            }
           ></textarea>
           <button
             className="primary-button send-request"
---
+++ src/pages/home/components/appointmentForm/appointmentForm.helpers.js
@@ -0,0 +1,36 @@
+import _get from 'lodash/get';
+
+export const EMAIL_REGEX =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const isValidEmail = email => EMAIL_REGEX.test(email);
+
+export const validateFormHelper = ({ formValues, displayError }) => {
+  if (!displayError) return {};
+
+  const formErrors = {};
+  const name = _get(formValues, "person_name");
+  const phone = _get(formValues, "phone_number");
+  const date = _get(formValues, "date");
+  const email = _get(formValues, "email");
+  const appointment = _get(formValues, "appointment");
+
+  if (!name) formErrors.person_name = "This filed is mandatory";
+  if (phone) {
+    if (phone.length !== 10)
+      formErrors.phone_number = "Phone number is not valid";
+  } else {
+    formErrors.phone_number = "This filed is mandatory";
+  }
+
+  if (email) {       
+    const isValid = isValidEmail(email);
+    if (!isValid) formErrors.email = "Email is not valid";
+  } else {
+    formErrors.email = "This filed is mandatory";
+  }
+
+  if (!date || !appointment) {
+    formErrors.date_appointment = "This filed is mandatory";
+  }
+  return formErrors;
+};
